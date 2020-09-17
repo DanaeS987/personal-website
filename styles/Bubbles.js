2701214de80897dea23c4c6b965d2226a5577615
DanaeS987 <71010992+DanaeS987@users.noreply.github.com>
2020-09-17 18:38:09 +0300

    Add files via upload

---
+++ styles/Bubbles.js
@@ -0,0 +1,35 @@
+function speechBubble1() {
+	
+	
+	var element = document.getElementById("sbubble1");
+	
+	if (element.style.display== 'none') {
+		element.style.display= 'block';
+	} else {
+		element.style.display='none';
+	}
+}
+
+function speechBubble2() {
+	
+	
+	var element = document.getElementById("sbubble2");
+	
+	if (element.style.display== 'none') {
+		element.style.display= 'block';
+	} else {
+		element.style.display='none';
+	}
+}
+
+function speechBubble3() {
+	
+	
+	var element = document.getElementById("sbubble3");
+	
+	if (element.style.display== 'none') {
+		element.style.display= 'block';
+	} else {
+		element.style.display='none';
+	}
+}
